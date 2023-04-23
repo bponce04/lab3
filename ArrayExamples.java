be50906f4ec3abd2f5dfcdc71b4979a8a6a85710
bponce04 <130109003+bponce04@users.noreply.github.com>
2023-04-23 13:53:20 -0700

    Debug
    Fixed two symptoms found in both reversed and reverseInPlace methods by rewriting two of the tests

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,14 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
+    for(int i = 0; i < arr.length/2; i += 1) {
+
+      int temp = arr[i];
+
       arr[i] = arr[arr.length - i - 1];
+
+      arr[arr.length - i -1] = temp;
+      
     }
   }
 
@@ -13,10 +19,23 @@ public class ArrayExamples {
   // order
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+
+    for (int i = 0; i < newArray.length; i ++) {
+
+      newArray[i] = arr[i];
+
     }
-    return arr;
+
+    for(int i = 0; i < newArray.length/2; i += 1) {
+
+      int temp = newArray[i];
+
+      newArray[i] = newArray[newArray.length - i - 1];
+
+      newArray[newArray.length - i - 1] = temp;
+
+    }
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,26 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlaceTwo() {
+    int[] input2 = {3,2,1};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {1,2,3}, input2);
+
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+
+  }
+
+  @Test
+  public void testReversedTwo() {
+
+    int[] input2 = {15,10,5};
+    assertArrayEquals(new int[]{5,10,15}, ArrayExamples.reversed(input2));
+
   }
 }
